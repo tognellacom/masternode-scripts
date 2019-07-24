0f42f4fd2e3db373aebef7f051a5072805ce2287
tognellap <patrick.tognella@tognella.com>
2019-07-24 08:39:26 +0200

    Update install.sh

--- TELOS/install.sh
+++ TELOS/install.sh
@@ -175,11 +175,6 @@ fi
 
 
 function checks() {
-if [[ $(lsb_release -d) != *18.04* ]]; then
-  echo -e "${RED}You are not running Ubuntu 16.04. Installation is cancelled.${NC}"
-  exit 1
-fi
-
 if [[ $EUID -ne 0 ]]; then
    echo -e "${RED}$0 must be run as root.${NC}"
    exit 1
